f3d4e01312f475238cd431a0a102b0d15221e2d7
Chowdhury Md Sami Al Muntahi <chowdhury.sami.al@gmail.com>
2019-12-12 12:10:42 +0600

    Jest and React-testing-library added

---
+++ components/Test/Interest.spec.tsx
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom/extend-expect";
+import Interest from './Interest';
+import { render } from '@testing-library/react';
+
+describe('Should render something', () => {
+  test('Should do something', () => {
+    const {container, debug} = render(<Interest text='Very much interested' />);
+    debug();
+    expect(container).toBeInTheDocument();
+  })
+})
---
+++ jest.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js"
+  ],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testMatch: [
+    "**/*.(test|spec).(ts|tsx)"
+  ],
+  globals: {
+    "ts-jest": {
+      babelConfig: require('./babel.config.js'),
+    }
+  },
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom/extend-expect"
+  ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+  ],
+  coverageReporters: [
+    "json",
+    "lcov",
+    "text",
+    "text-summary"
+  ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/mocks.js",
+    "\\.(css|less|scss)$": "<rootDir>/__mocks__/mocks.js"
+  }
+};
