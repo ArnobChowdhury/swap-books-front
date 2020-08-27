b4f217fab20b8c78c753958e1bae3a482cbad80b
ArnobChowdhury <chowdhury.sami.al@gmail.com>
2020-08-28 00:48:36 +0600

    added react persistence

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,25 @@ import React, { FC } from 'react';
 import { AppProps } from 'next/app';
 import { wrapper } from 'redux/store';
 import { ThemeProvider } from 'styled-components';
+import { PersistGate } from 'redux-persist/integration/react';
+import { useStore } from 'react-redux';
 import GlobalStyles from '../components/GlobalStyles';
 import theme from '../theme';
 
 // todo
 // eslint-disable-next-line react/prop-types
-const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
+  const store = useStore();
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {/**
+       @ts-ignore*/}
+      <PersistGate persistor={store.__persistor} loading={null}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </ThemeProvider>
+  );
+};
 
 export default wrapper.withRedux(WrappedApp);
--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,34 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, Middleware } from 'redux';
 import thunk from 'redux-thunk';
-import { MakeStore, Context, createWrapper } from 'next-redux-wrapper';
+import { MakeStore, createWrapper } from 'next-redux-wrapper';
 import rootReducer, { RootState } from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const makeStore: MakeStore<RootState> = (context: Context = {}) => {
-  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const persistConfig = {
+  key: 'bookswap',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const bindMiddlewares = (middleware: Middleware[]) => {
+  if (process.env.NODE_ENV === 'development') {
+    return composeWithDevTools(applyMiddleware(...middleware));
+  } else {
+    return applyMiddleware(...middleware);
+  }
+};
+
+const makeStore: MakeStore<RootState> = () => {
+  if (!process.browser) {
+    return createStore(rootReducer, bindMiddlewares([thunk]));
+  }
+  const store = createStore(persistedReducer, bindMiddlewares([thunk]));
+  // @ts-ignore
+  store.__persistor = persistStore(store);
+  return store;
 };
 
 export const wrapper = createWrapper<RootState>(makeStore, { debug: true });
