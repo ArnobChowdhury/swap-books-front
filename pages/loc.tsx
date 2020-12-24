0d80fac107afb9bd0c2046b31922ddd1f44482ab
ArnobChowdhury <chowdhury.sami.al@gmail.com>
2020-12-25 00:08:08 +0600

    added features to map

--- pages/loc.tsx
+++ pages/loc.tsx
@@ -1,9 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { initiateMap } from '../public/loc';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'redux/reducers';
 import { updateUserLocationReq } from 'redux/actions/user';
+import { Map, LeafletMouseEvent, DragEndEvent, LatLng, Marker } from 'leaflet';
+
+// Used LeafletMouseEvent interface with the event of Geocoder on select event.
+// This is a fake event.
+interface Geocoder {
+  on(event: 'select', cb: (event: LeafletMouseEvent) => void): void;
+}
 
 function createScriptTag(source: string, cb?: () => void): void {
   const script = document.createElement('script');
@@ -47,21 +54,88 @@ const Geo = () => {
   const numOfScripts = scriptSources.length - 1;
   const scriptCBs: { (): void }[] = [];
 
+  const [locIQMap, setLocIQMap] = useState<Map | null>();
+  const [locIQMarker, setLocIQMarker] = useState<Marker | null>();
+  const [geoCoder, setGeoCoder] = useState<Geocoder | null>();
+
   for (let i = numOfScripts; i >= 0; i--) {
     if (i == numOfScripts) {
       scriptCBs[i] = () =>
-        createScriptTag(scriptSources[i], () => initiateMap(userLat, userLon));
+        createScriptTag(scriptSources[i], () =>
+          initiateMap(userLat, userLon, setLocIQMap, setLocIQMarker, setGeoCoder),
+        );
     } else {
       scriptCBs[i] = () => createScriptTag(scriptSources[i], scriptCBs[i + 1]);
     }
   }
 
+  const [currentLoc, setCurrentLoc] = useState<{
+    lat: number | null;
+    lng: number | null;
+  } | null>({ lat: userLat, lng: userLon });
+
+  const moveMarker = (map: Map, marker: Marker, latlng: LatLng) => {
+    map.setView(latlng, map.getZoom());
+    // @ts-ignore
+    marker.setLatLng(latlng, { draggable: true });
+    setCurrentLoc(latlng);
+  };
+
+  const handleClick = (
+    event: LeafletMouseEvent,
+    locIQMap: Map,
+    locIQMarker: Marker,
+  ) => {
+    moveMarker(locIQMap, locIQMarker, event.latlng);
+  };
+
+  const handleAutoCompleteSelection = (
+    event: LeafletMouseEvent,
+    locIQMap: Map,
+    locIQMarker: Marker,
+  ) => {
+    moveMarker(locIQMap, locIQMarker, event.latlng);
+  };
+
+  const handleMarkerDrag = (
+    _event: DragEndEvent,
+    locIQMap: Map,
+    locIQMarker: Marker,
+  ) => {
+    const position = locIQMarker.getLatLng();
+    moveMarker(locIQMap, locIQMarker, position);
+  };
+
+  useEffect(() => {
+    if (locIQMap && locIQMarker && geoCoder) {
+      locIQMap.on('click', (e: LeafletMouseEvent) =>
+        handleClick(e, locIQMap, locIQMarker),
+      );
+      geoCoder.on('select', event => {
+        handleAutoCompleteSelection(event, locIQMap, locIQMarker);
+      });
+      locIQMarker.on('dragend', event => {
+        handleMarkerDrag(event, locIQMap, locIQMarker);
+      });
+      locIQMarker
+        .bindPopup(`Lat: ${currentLoc?.lat} Lng: ${currentLoc?.lng}`, {
+          closeOnClick: false,
+        })
+        .openPopup();
+    }
+  }, [locIQMap, locIQMarker, geoCoder, currentLoc]);
+
+  const [scriptActivatedOnce, setScriptActivatedOnce] = useState<boolean>(false);
   const activateScripts = scriptCBs[0];
   useEffect(() => {
-    if (userLon && userLat) {
+    if (userLon && userLat && !scriptActivatedOnce) {
       activateScripts();
+      setScriptActivatedOnce(true);
     }
-  }, [userLon, userLat]);
+  }, [userLon, userLat, scriptActivatedOnce]);
+
+  const positionHasChanged =
+    userLon !== currentLoc?.lng || userLat !== currentLoc.lat;
 
   return (
     <>
@@ -101,6 +175,16 @@ const Geo = () => {
             boxShadow: '2px 2px 2px rgba(0,0,0,.2)',
           }}
         ></div>
+        <button
+          onClick={() => {
+            if (currentLoc?.lat && currentLoc.lng) {
+              dispatch(updateUserLocationReq(currentLoc.lng, currentLoc.lat));
+            }
+          }}
+          disabled={!positionHasChanged}
+        >
+          Save
+        </button>
       </div>
     </>
   );
--- public/loc.js
+++ public/loc.js
@@ -1,14 +1,24 @@
 // eslint-disable-next-line
 let key = 'pk.cf2d4ca311f0b700ddd1ac21fb730434';
+let map;
+let marker;
 
-export const initiateMap = (userLat, userLong) => {
-  const map = L.map('map', {
+export const initiateMap = (
+  userLat,
+  userLon,
+  attachMapFunc,
+  attachMarkerFunc,
+  attachGeoCoderFunc,
+) => {
+  map = L.map('map', {
     // center: [39.73, -104.99], // Map loads with this location as center
-    center: [userLat, userLong],
-    zoom: 14,
+    center: [userLat, userLon],
+    zoom: 13,
     scrollWheelZoom: true,
     zoomControl: false,
   });
+
+  marker = L.marker([userLat, userLon], { draggable: true }).addTo(map);
 
   L.tileLayer(
     `https://{s}-tiles.locationiq.com/v2/obk/r/{z}/{x}/{y}.png?key=${key}`,
@@ -20,14 +30,20 @@ export const initiateMap = (userLat, userLong) => {
     })
     .addTo(map);
 
-  L.control
+  const geocoder = L.control
     .geocoder(key, {
-      // placeholder: 'Search nearby',
+      placeholder: 'Search nearby',
       url: 'https://api.locationiq.com/v1',
       expanded: true,
       panToPoint: true,
       focus: true,
       position: 'topleft',
+      markers: false,
     })
     .addTo(map);
+
+  // map.on('click', handleClick);
+  attachMapFunc(map);
+  attachMarkerFunc(marker);
+  attachGeoCoderFunc(geocoder);
 };
